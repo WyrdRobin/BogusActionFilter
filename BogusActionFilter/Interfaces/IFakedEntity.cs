5f3d24bb1366809e3cf876af404df973a9829ab9
WyrdRobin <WyrdRobin@gmail.com>
2023-07-27 13:25:56 -0500

    Add entity interface so we can manage rules on a per entity level

---
+++ BogusActionFilter/Interfaces/IFakedEntity.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+
+namespace BogusActionFilter.Interfaces
+{
+    /// <summary>
+    /// Interface used by BogusAutoFilter to get rules for generating the entity when its a return type on the applied controller
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IFakedEntity<T> where T : class
+    {
+        /// <summary>
+        /// Returns a Faker<T> instance of the main entity that the interface is applied to so rules can be set on an entity level
+        /// </summary>
+        /// <returns>Faker<typeparamref name="T"/></returns>
+        public Faker<T> GetFaker();
+    }
+}
--- BogusActionFilter/Models/BogusActionFilterConfiguration.cs
+++ BogusActionFilter/Models/BogusActionFilterConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BogusActionFilter.Models
+﻿namespace BogusActionFilter.Models
 {
     /// <summary>
     /// Configuration class for turning on the action filter and other configuration settings
