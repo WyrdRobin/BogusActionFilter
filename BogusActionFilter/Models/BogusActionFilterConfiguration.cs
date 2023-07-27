a014f3bdd26acf3c8e7638038417e1e66677d8de
WyrdRobin <WyrdRobin@gmail.com>
2023-07-27 13:21:01 -0500

    Add strongly typed configuration object

---
+++ BogusActionFilter/Models/BogusActionFilterConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogusActionFilter.Models
+{
+    /// <summary>
+    /// Configuration class for turning on the action filter and other configuration settings
+    /// </summary>
+    public class BogusActionFilterConfiguration
+    {
+        /// <summary>
+        /// Tells the filter to replace response data from the controllers with data generated in Faker
+        /// </summary>
+        public bool IsActive { get; set; }
+        /// <summary>
+        /// When the return type is an ennumerable or list this determins the default number of records to return
+        /// </summary>
+        public int RecordReturnCount { get; set; }
+    }
+}
